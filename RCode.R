f2578734935bdfc85f3b5ad69c51302871529789
popolinka <orkun.nazim.kadioglu@hu-berlin.de>
2019-12-02 20:49:04 +0100

    distance matrix kodu ve de pdf i

--- RCode.R
+++ RCode.R
@@ -160,8 +160,30 @@ data_agg <-aggregate(data_comb[,-c(1)], by=list(data_comb$Group.1), mean, na.rm=
 data_agg
 dist.df <- dist(data_agg[,-c(1)])
 dist.df
+
+dst <- data.matrix(dist.df)
+dim <- ncol(dst)
+
+# distance/similarity matrix grafik cizme
+# Open a pdf file
+# df("distanceMatrix.pdf") # bu kodu run ettikten sonra kapatmassan, grafik gozukmez R'de sadece dosyaya kaydeder
+# eger grafigi gormek istiyosan bu kodu run etme!
+
+# Create a plot
+image(1:dim, 1:dim, dst, axes = FALSE, xlab="", ylab="", 
+      col = gray.colors(12, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL, rev = FALSE))
+
+axis(1, 1:dim, data_agg[1:20,1], cex.axis = 0.5, las=3)
+axis(2, 1:dim, data_agg[1:20,1], cex.axis = 0.5, las=1)
+text(expand.grid(1:dim, 1:dim), sprintf("%0.1f", dst), cex=0.6)
+
+# Close the pdf file
+# dev.off() 
+
 fit <- cmdscale(dist.df, k = 2)
 fit
+
+
 
 x <- fit[,1]
 y <- fit[,2]
