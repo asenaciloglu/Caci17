2f7aaf671a9db85a86ab6bd9d454e1d00d966224
asenaciloglu <asena.ciloglu@gmail.com>
2019-12-17 19:59:32 +0100

    dddd

--- SWP3/SWP3Code.R
+++ SWP3/SWP3Code.R
@@ -1,2 +1,9 @@
-#yeni kod
-#summary
+setwd("~/Caci17/SWP3")
+bluetooth <- read.csv("indivData.csv")
+
+nrow(bluetooth)
+summary(bluetooth)
+str(bluetooth)
+
+seg.summ <- function (data , groups) 
+{aggregate (data , list(groups), function (x) mean(as.numeric (x)))}
